e011dfcd8efb9b56ae95258a09c67456e2641943
Vaxee03 <012jas84@gmail.com>
2024-10-21 21:46:23 +0900

    Feat : 정답 숫자 생성 기능 추가
    중복 없는 세자리 수의 정답 숫자를 생성하여 정해진 숫자를 resultNum에 저장.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        Set<Integer> resultNumCreate = new HashSet<>();
+        Random random = new Random();
+
+        while (resultNumCreate.size() < 3) {
+            resultNumCreate.add(random.nextInt(9) + 1);
+        }
+
+        ArrayList<Integer> resultNum = new ArrayList<>(resultNumCreate);
+        Collections.shuffle(resultNum);
+        System.out.println("선택된 번호는 " + resultNum + " 입니다.");
     }
 }
