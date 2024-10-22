598c42b28f5fce5f7ed321206b23621ba5bc1f0e
Vaxee03 <012jas84@gmail.com>
2024-10-22 12:17:03 +0900

    Style : 클래스 분리 작업 실행
    BaseballGame 클래스 생성 후 기능 이전

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Set<Integer> resultNumCreate = new HashSet<>();
-        Random random = new Random();
+    BaseballGame baseball = new BaseballGame();
+    baseball.play();
 
-        while (resultNumCreate.size() < 3) {
-            resultNumCreate.add(random.nextInt(9) + 1);
-        }
-
-        ArrayList<Integer> resultNum = new ArrayList<>(resultNumCreate);
-        Collections.shuffle(resultNum);
-        System.out.println("선택된 번호는 " + resultNum + " 입니다.");
     }
 }
