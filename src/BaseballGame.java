fb71f3152ee2e6ba6941a00bdcff325a879c2644
Vaxee03 <012jas84@gmail.com>
2024-10-22 17:59:00 +0900

    Feat : 스트라이크 개수 계산 기능 추가
    스트라이크 개수 계산 메서드 생성 및 기능 활성화

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 public class BaseballGame {
     Scanner sc = new Scanner(System.in);
-    int gameCount;
+    Set<Integer> resultNum = new LinkedHashSet<>();
+    Random random = new Random();
+    int gameCount = 0;
 
 
     // 객체 생성시 정답을 만들도록 함
     public BaseballGame() {
 
-
-        Set<Integer> resultNumCreate = new LinkedHashSet<>();
-        Random random = new Random();
-
-        // 정답 숫자 생성
-        while (resultNumCreate.size() < 3) {
-            resultNumCreate.add(random.nextInt(9) + 1);
+        while (resultNum.size() < 3) {
+            resultNum.add(random.nextInt(9) + 1);
         }
     }
 
@@ -26,15 +23,16 @@ public class BaseballGame {
             String num = sc.nextLine();
 
             // 2. 올바른 입력값을 받았는지 검증
-            if(!validateInput(num)){
+            if (!validateInput(num)) {
                 System.out.println("잘못된 입력입니다. 다시 시도해주십시오.");
                 continue;
             }
 
             // 3. 게임 진행횟수 증가
-            gameCount ++;
+            gameCount++;
 
             // 4. 스트라이크 개수 계산
+            int strike = countStrike(num);
             // 5. 정답여부 확인, 만약 정답이면 break 를 이용해 반복문 탈출
             // 6. 볼 개수 계산
             // 7. 힌트 출력
@@ -43,15 +41,26 @@ public class BaseballGame {
     }
 
     protected boolean validateInput(String input) {
-    if (input.length() != 3) return false;
-    return input.chars().distinct().count() == 3;
+        if (input.length() != 3) return false;
+        return input.chars().distinct().count() == 3;
     }
 
-//    private int countStrike(String input) {
-//
-//    }
-//
-//    private int countBall(String input) {
-//
-//    }
+    // 스트라이크 개수 계산
+    private int countStrike(String input) {
+        int strike = 0;
+
+        List<Integer> answerList = new ArrayList<>(resultNum);
+
+        for (int i = 0; i < 3; i++) {
+            if (Character.getNumericValue(input.charAt(i)) == answerList.get(i)){
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+
+    private int countBall(String input) {
+
+    }
 }
