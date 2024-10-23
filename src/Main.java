1d3d4bac289e819041db13fffc1c96cab5bf8c47
Vaxee03 <012jas84@gmail.com>
2024-10-23 15:17:27 +0900

    Feat : 시작 안내 메뉴, 게임 기록 보기 기능 추가
    시작 메뉴 추가 및 시작 메뉴를 통한 게임 기록 보기 추가
    정답을 맞출시에 다시 시작 메뉴로 돌아감

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-    BaseballGame baseball = new BaseballGame();
-    baseball.play();
+        // 인스턴트 화
+        Scanner sc = new Scanner(System.in);
+        BaseballGame baseball = new BaseballGame();
+        List<Integer> scoreboard = new ArrayList<>();
 
+        while (true) {
+            System.out.println("환영합니다! 원하시는 번호를 입력하여주세요!");
+            System.out.println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
+            int start = sc.nextInt();
+            if (start == 1) {
+                int gameCount = baseball.play();
+                System.out.println("정답입니다! 당신은 " + gameCount + " 회 만에 정답을 맞추셨습니다!");
+                System.out.println("─────────────────────────────────────────────────────────");
+                scoreboard.add(gameCount);
+            } else if (start == 2) {
+                for (int i = 0; i < scoreboard.size(); i++) {
+                    System.out.println((i + 1) + "번째 게임 : 시도 횟수 - " + scoreboard.get(i));
+                }
+                System.out.println("─────────────────────────────────────────────────────────");
+            } else {
+                System.out.println("게임이 종료되었습니다!");
+                break;
+            }
+        }
     }
 }
+
