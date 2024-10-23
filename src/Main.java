1e8d2d27fc9e3290d5ae96ef19818bc0b765c324
Vaxee03 <012jas84@gmail.com>
2024-10-23 15:55:25 +0900

    Feat : 시작 메뉴 오류메시지 추가
    시작 메뉴에서 1,2,3 이외의 입력값에 대해서는 오류메시지를 노출하는 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,30 @@ public class Main {
         while (true) {
             System.out.println("환영합니다! 원하시는 번호를 입력하여주세요!");
             System.out.println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
-            int start = sc.nextInt();
-            if (start == 1) {
+            String start = sc.nextLine();
+
+            if (!checkMenuError(start)){
+                System.out.println("올바른 숫자를 입력해주세요!");
+            }
+
+            if (start.equals("1")) {
                 int gameCount = baseball.play();
                 System.out.println("정답입니다! 당신은 " + gameCount + " 회 만에 정답을 맞추셨습니다!");
                 System.out.println("─────────────────────────────────────────────────────────");
                 scoreboard.add(gameCount);
-            } else if (start == 2) {
+            } else if (start.equals("2")) {
                 for (int i = 0; i < scoreboard.size(); i++) {
                     System.out.println((i + 1) + "번째 게임 : 시도 횟수 - " + scoreboard.get(i));
                 }
                 System.out.println("─────────────────────────────────────────────────────────");
-            } else {
+            } else if (start.equals("3")) {
                 System.out.println("게임이 종료되었습니다!");
                 break;
             }
         }
+    }
+    protected static boolean checkMenuError(String start) {
+        return start.equals("1") || start.equals("2") || start.equals("3");
     }
 }
 
